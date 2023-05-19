a4ea4a0920ff741028fbd8bd6df2e169e0a5654f
Willy Vega <willy.vegaa@Willys-MBP-2.lan>
2023-05-19 10:05:51 -0400

    created axiosWithAuth in index.js

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -1,1 +1,11 @@
 // ✨ implement axiosWithAuth
+import axios from 'axios';
+
+export const axiosWithAuth = () => {
+    const token = localStorage.getItem('token');
+
+    return axios.create({
+        baseURL: 'http://localhost:9000/api',
+        headers: { authorization: token }
+    })
+}
